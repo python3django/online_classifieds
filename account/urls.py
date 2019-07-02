9e7f5a3fb030d480305090f4b572734efc3ef64d
python3django <obel60135@gmail.com>
2019-07-02 21:47:48 +0300

    Updated regisration (sent links with token an email)

--- account/urls.py
+++ account/urls.py
@@ -2,11 +2,16 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
+from django.conf.urls import url
+
 
 urlpatterns = [
 	path('', include('django.contrib.auth.urls')),
 	path('', views.dashboard, name='dashboard'),
 	path('register/', views.register, name='register'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate_account, name='activate'),
+    #path('activate/<str:uid>/<str:token>', views.activate_account, name='activate'),
     path('edit/', views.edit, name='edit'),
 ]
 
--- account/views.py
+++ account/views.py
@@ -5,6 +5,15 @@ from django.contrib import messages
 from classifieds.models import Note
 from .models import Profile
 
+from django.http import HttpResponse
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .token_generator import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
 
 @login_required
 def dashboard(request):
@@ -20,11 +29,21 @@ def register(request):
             new_user = user_form.save(commit=False)
             # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password'])
+            new_user.is_active = False
             # Save the User object
             new_user.save()
-            # Create the user profile
-            Profile.objects.create(user=new_user)
-            return render(request, 'account/register_done.html', {'new_user': new_user})
+            current_site = get_current_site(request)
+            email_subject = 'Activate Your Account'
+            message = render_to_string('account/activate_account.html', {
+                'user': new_user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(new_user.pk)),
+                'token': account_activation_token.make_token(new_user),
+            })
+            to_email = user_form.cleaned_data.get('email')
+            email = EmailMessage(email_subject, message, to=[to_email])
+            email.send()
+            return render(request, 'account/sent_activation_link.html')            
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
@@ -46,4 +65,19 @@ def edit(request):
         profile_form = ProfileEditForm(instance=request.user.profile)
     return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
 
+
+def activate_account(request, uidb64, token):
+    try:
+        uid = force_bytes(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        # Create the user profile
+        Profile.objects.create(user=user)
+        return render(request, 'account/register_done.html', {'new_user': user})
+    else:
+        return render(request, 'account/activation_link_invalid.html')
 
